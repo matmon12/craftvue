57d76bf3fe009dbd1f91fe5aeb28a4db9f06ce57
Matvey <matveym151@gmail.com>
2025-10-20 22:26:48 +0300

    added base path

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -11,6 +11,7 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/craftvue/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
